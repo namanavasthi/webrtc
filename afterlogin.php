fa132b3f9201d4a0fadea3f4a229f76d800d6352
NAMAN AVASTHI <naman.avasthi@gmail.com>
2014-12-17 19:38:31 +0530

    first push

---
+++ afterlogin.php
@@ -0,0 +1,154 @@
+<?php
+session_start();
+$at = $_SESSION['accesstoken'] ;
+
+print $at;
+
+?>
+
+<html xmlns:fb="https://www.facebook.com/2008/fbml">
+  <head> 
+    <title> 
+      AFTER LOGIN
+    </title> 
+  </head> 
+<body> 
+    
+<div id="fb-root"></div>
+<div id="user-info"></div>
+<p><button id="fb-auth">Login</button></p>
+
+<script>
+window.fbAsyncInit = function() {
+  FB.init({ appId: '955536087794562', 
+        status: true, 
+        cookie: true,
+        xfbml: true,
+        oauth: true});
+function updateButton(response) {
+    var button = document.getElementById('fb-auth');
+        
+    if (response.authResponse) {
+      //user is already logged in and connected
+      var userInfo = document.getElementById('user-info');
+      FB.api('/me', function(response) {
+        userInfo.innerHTML = '<img src="https://graph.facebook.com/' 
+      + response.id + '/picture">' + response.name;
+        button.innerHTML = 'Logout';
+      });
+
+
+
+      //add FB.api for friends here
+      var userInfo2 = document.getElementById('user-info');
+      FB.api("/me/friends", function(response) {
+        if (response && !response.error) {
+        userInfo2.innerHTML = '<img src="https://graph.facebook.com/' 
+      + response.id + '/picture">' + response.name;
+        button.innerHTML = 'Logout';
+    }
+      });
+
+
+
+
+      button.onclick = function() {
+        FB.logout(function(response) {
+          var userInfo = document.getElementById('user-info');
+          userInfo.innerHTML="";
+    });
+      };
+    }else {
+      //user is not connected to your app or logged out
+      button.innerHTML = 'Login';
+      button.onclick = function() {
+        FB.login(function(response) {
+      if (response.authResponse) {
+            FB.api('/me', function(response) {
+          var userInfo = document.getElementById('user-info');
+          userInfo.innerHTML = 
+                '<img src="https://graph.facebook.com/' 
+            + response.id + '/picture" style="margin-right:5px"/>' 
+            + response.name;
+        });    
+          } else {
+            //user cancelled login or did not grant authorization
+          }
+        }, {scope:'email'});    
+      }
+    }
+  }
+
+  // run once with current status and whenever the status changes
+  FB.getLoginStatus(updateButton);
+  FB.Event.subscribe('auth.statusChange', updateButton);    
+};
+    
+(function() {
+  var e = document.createElement('script'); e.async = true;
+  e.src = document.location.protocol 
+    + '//connect.facebook.net/en_US/all.js';
+  document.getElementById('fb-root').appendChild(e);
+}());
+
+</script>
+</body> 
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+#bullshit
+    $token_url = "https://graph.facebook.com/oauth/access_token?type=web_server&client_id="
+        . $app_id . "&redirect_uri=http://trial-fypgroup11.rhcloud.com/facebookcallback.php&client_secret="
+        . $app_secret . "&code=" . $code;
+
+    $access_token = file_get_contents($token_url);
+
+    $_SESSION['accesstoken'] = $access_token;
+    $_SESSION['code'] = $code;
+    $_SESSION["facebook"] = "true";
+
+#end of bullshit
+
+    $url2 = 'https://graph.facebook.com/me/friends?';
+    $json2 = file_get_contents($url2.$access_token);
+    $data2 = json_decode($json2,true);
+
+
+    $friends=$data2;
+
+     print 'Friends On WebRTC Trial : ';
+     print '<br>';
+     foreach($friends['data'] as $key=>$value)
+     {
+            print $value['name'];
+            print '<br>';
+     }
+
+
+   
+
+
+?>
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Trial</title>
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+<a href="http://trial-fypgroup11.rhcloud.com/try.html">FOR SESSION</a>
+</head> 
+<body> 
---
+++ facebookcallback.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Some Site</title>
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+</head> 
+<body> 
+
+<?php 
+
+
+
+
+    $app_id = "955536087794562";
+    $app_secret = "d5a0d6381787072586f2f6849183f51d";
+    
+    $code = $_GET["code"];
+	$error = $_GET["error_reason"];
+	if ($error == "user_denied") {
+?>
+			<p>I'm sorry you weren't interested in using Facebook.</p>
+            <a href="http://trial-fypgroup11.rhcloud.com">Go Back To Main Page</a>
+			
+				
+<?php
+		
+	} else {
+
+    $token_url = "https://graph.facebook.com/oauth/access_token?type=web_server&client_id="
+        . $app_id . "&redirect_uri=http://trial-fypgroup11.rhcloud.com/facebookcallback.php&client_secret="
+        . $app_secret . "&code=" . $code;
+
+    $access_token = file_get_contents($token_url);
+
+	$_SESSION['accesstoken'] = $access_token;
+	$_SESSION['code'] = $code;
+ 	$_SESSION["facebook"] = "true";
+
+    $accesstoken=$_SESSION['accesstoken'];
+    $_SESSION['accesstoken'] = $_POST['accesstoken']
+
+
+	$url1 = 'https://graph.facebook.com/me/?';
+	$url2 = 'https://graph.facebook.com/me/friends?';
+    $json1 = file_get_contents($url1.$access_token);
+    $data1 = json_decode($json1,true);
+
+    $json2 = file_get_contents($url2.$access_token);
+    $data2 = json_decode($json2,true);
+    $va1=$json1;
+    $va2=$json2;
+    #print_r($json1);
+    #print_r($var2);
+    
+    print $data1['first_name'];
+    print '<br>';
+    print $data1['email'];
+    print '<br>';
+
+
+
+    $friends=$data2;
+
+     print 'Friends On WebRTC Trial : ';
+     print '<br>';
+     foreach($friends['data'] as $key=>$value)
+     {
+            print $value['name'];
+            print '<br>';
+     }
+
+    # $json1="muhahaha (this is aweomse asdasd)";
+    # $json2="muhahaha (this is aweomse asdasd)";
+
+
+    echo '<a href="http://trial-fypgroup11.rhcloud.com/afterlogin.php?data1='.$json1.'&data2='.$json2.'">Link 2</a>';
+    echo '<a href="http://finalyearproject11.comule.com/">Link for WebRTC</a>';
+
+
+
+
+			
+	    } 
+
+?>
+
+
+
+
+
+
+	
+	
