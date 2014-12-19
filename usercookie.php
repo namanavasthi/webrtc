367ab4eefbbfe380e95edd942fee4b7130a53e64
NAMAN AVASTHI <naman.avasthi@gmail.com>
2014-12-19 11:51:57 +0530

    session changes

--- usercookie.php
+++ usercookie.php
@@ -1,5 +1,13 @@
 <?php
 print '<br>';
-print_r($_COOKIE);
+#print_r($_COOKIE);
+
+if(isset($_COOKIE['userdata'])){
+	foreach($_COOKIE['userdata'] as $name=>$value){
+		$name=htmlspecialchars($name);
+		$value=htmlspecialchars($value);
+		echo "$name : $value <br />\n";
+	}
+}
 
 ?>
