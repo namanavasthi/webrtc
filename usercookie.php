ad553c2b00560a8e88f83a3154f9ecaee26f33d2
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-01-11 22:04:09 +0530

    try main page

--- usercookie.php
+++ usercookie.php
@@ -6,7 +6,7 @@ if(isset($_COOKIE['userdata'])){
 	foreach($_COOKIE['userdata'] as $name=>$value){
 		$name=htmlspecialchars($name);
 		$value=htmlspecialchars($value);
-		echo "$name : $value <br />\n";
+		// echo "$name : $value <br />\n";
 	}
 }
 
