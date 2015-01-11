d98243d71da7ab71ff9501fe91756c04a95bf5b6
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-01-11 23:25:06 +0530

    try main page

--- ElasticSVGElements/kidding.php
+++ ElasticSVGElements/kidding.php
@@ -280,7 +280,7 @@ print_r($_COOKIE['userdata']);
 			<div class="side-fluid">
 				<div class="intro-content">
 					<p>name is : </p>
-					<?php print_r($_COOKIE['userdata']); ?>
+					<?php print_r($_COOKIE['userdata']['name']); ?>
 					<div class="profile"><img src="img/profile1.jpg" alt="profile1"></div>
 					<!-- <a href="http://www.google.com"><h1><span>Toby Blue </span><span>Web Designer</span></h1></a> -->
 					<a href="http://www.google.com"><h1><span><? echo $name ?></span><span>Web Designer</span></h1></a>
