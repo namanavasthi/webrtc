f608d480178a215bd92a0decff5ef6acca17f4f9
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-01-12 20:22:23 +0530

    recursive loop

---
+++ checkchanges.php
@@ -0,0 +1,29 @@
+<?php
+
+$flag=0;
+$name="Naman";
+
+$connect = mysql_connect("127.2.139.130","adminPfy2zVu","BXXbBfmR7fWS");
+
+if (!$connect) {
+	die("Connection failed: " .mysql_error());
+} 
+echo 'Connected successfully';
+
+//connect to the datatbase
+mysql_select_db("webrtc");
+
+$query = "SELECT * FROM users (firstname,lastname,fullname,emailid,hashemail,username) WHERE firstname = '".$name."'");
+
+while($flag>0){
+	$result = mysql_query($query);
+
+	if(mysql_num_rows($result) != 0) {
+        echo "query executed succesfully";
+        $flag=2;
+	}
+}
+
+
+
+?>
