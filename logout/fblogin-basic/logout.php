816d1bf33c5c499d7e2ca63663b6cc3ba807ee71
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-02-22 20:02:09 +0530

    logout button

--- logout/fblogin-basic/logout.php
+++ logout/fblogin-basic/logout.php
@@ -1,4 +1,17 @@
 <?php
+require_once( 'lib/Facebook/FacebookRedirectLoginHelper.php');
+
+use Facebook\FacebookRedirectLoginHelper;
+
+$redirect_url='http://webrtc-fypgroup11.rhcloud.com/logout/fblogin-basic/';
+$helper = new FacebookRedirectLoginHelper($redirect_url);
+$sess = $helper->getSessionFromRedirect();
+echo "this is sess :";
+echo $sess;
+
+
+
+
 $next = 'http://webrtc-fypgroup11.rhcloud.com/logout/fblogin-basic/?&logout=true';
 $link = $helper->getLogoutUrl($sess,$next);
 echo '<a href="'.$link.'" ><button>Logout from facebook</button></a>';
