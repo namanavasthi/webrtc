51d283f167506c0ed4a9e3ba1109654265c6974c
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-02-22 17:35:30 +0530

    logout button

--- ElasticSVGElements/try_logout.php
+++ ElasticSVGElements/try_logout.php
@@ -2,7 +2,14 @@
 
 session_start();
 
-include 'libs/facebook-php-sdk-v4-4.0-dev/autoload.php';
+// include 'libs/facebook-php-sdk-v4-4.0-dev/autoload.php';
+define('FACEBOOK_SDK_V4_SRC_DIR', '/libs/facebook-php-sdk-v4-4.0-dev/src/Facebook/');
+require __DIR__ . '/libs/facebook-php-sdk-v4-4.0-dev/autoload.php';
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
 
 echo "success";
 
