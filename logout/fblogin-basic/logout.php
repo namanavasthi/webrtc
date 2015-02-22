abb47ef85a4151615ddab836cba5a8303a343424
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-02-22 19:39:34 +0530

    logout button

--- logout/fblogin-basic/logout.php
+++ logout/fblogin-basic/logout.php
@@ -1,7 +1,7 @@
 <?php
-
+setcookie('fbs_'.$helper->getAppId(), '', time()-100, '/', 'webrtc-fypgroup11.rhcloud.com');
 unset($_SESSION['fb_token']);
 session_destroy();
-header('Location: http://www.google.com');
+// header('Location: http://www.google.com');
 
 ?>
