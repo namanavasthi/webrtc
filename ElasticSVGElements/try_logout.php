48988b0b9d3111c5233901b79c735ebcec2c5a32
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-02-22 17:30:23 +0530

    logout button

--- ElasticSVGElements/try_logout.php
+++ ElasticSVGElements/try_logout.php
@@ -4,7 +4,20 @@ session_start();
 
 require_once 'libs/facebook-php-sdk-v4-4.0-dev/autoload.php';
 
+echo "success";
+
+// $facebook->destroySession();
+// $logout = $facebook->getLogoutUrl();
+// $facebook->setAccessToken('');
+
+
 $logoutUrl = $facebook->getLogoutURL();
 	echo "<a href='$logoutUrl'>LOG OUT BITCH </a>";
 
 ?>
+
+<html>
+<head>
+	<title>logout_try</title>
+</head>
+</html>
