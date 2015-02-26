6f0e2eb84d0d9c8879f98505598f637444c0e0d0
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-02-26 22:28:39 +0530

    fb thing change

--- finalstuffvideo1/Finalishstuff/index.php
+++ finalstuffvideo1/Finalishstuff/index.php
@@ -276,17 +276,32 @@
 
 
 
-            $friends_list = (new FacebookRequest( $sess, 'GET', '/me/friends' ))->execute()->getGraphObject()->asArray();
-            // output response
-            // echo '<pre>' . print_r( $friends_list, 1 ) . '</pre>';
-            // output total friends
-            // echo count( $friends_list['data'] ); 
+            // $friends_list = (new FacebookRequest( $sess, 'GET', '/me/friends' ))->execute()->getGraphObject()->asArray();
+            // // output response
+            // // echo '<pre>' . print_r( $friends_list, 1 ) . '</pre>';
+            // // output total friends
+            // // echo count( $friends_list['data'] ); 
 
-            $friends = $friends_list['data'];
-            foreach ($friends as $key => $value) {
-              $friends[$key] = (array)$value;
-            }
+            // $friends = $friends_list['data'];
+            // foreach ($friends as $key => $value) {
+            //   $friends[$key] = (array)$value;
+            // }
 
+
+            // Check user's token is valid or not.
+$me = (new \Facebook\FacebookRequest(
+$sess, 'GET', '/me/friends'
+))->execute()->getGraphObject(\Facebook\GraphUser::className());
+
+$result = $me->asArray();
+
+// Get user's friends
+$friends = $result['data'];
+
+// Converting classes to array
+foreach ($friends as $key => $value) {
+    $friends[$key] = (array)$value;
+}
 
 
 
