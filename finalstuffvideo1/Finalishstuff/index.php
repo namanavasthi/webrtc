4f142cb05cd9d8d47241e5e863786fd6c8a570d5
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-02-26 19:49:19 +0530

    fb thing change

--- finalstuffvideo1/Finalishstuff/index.php
+++ finalstuffvideo1/Finalishstuff/index.php
@@ -127,7 +127,7 @@
   //2.Use app id,secret and redirect url 
   $app_id = '955536087794562';
   $app_secret = 'd5a0d6381787072586f2f6849183f51d';
-  $redirect_url='http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/.php';
+  $redirect_url='http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/homepage.php';
 
   //3.Initialize application, create helper object and get fb sess
    FacebookSession::setDefaultApplication($app_id,$app_secret);
@@ -140,7 +140,7 @@
   }
 
   //logout
-  $logout = 'http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/?&logout=true'; //remove index.php if error arrises
+  $logout = 'http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/index.php?&logout=true'; //remove index.php if error arrises
 
   //4. if fb sess exists echo name 
     if(isset($sess)){
@@ -169,7 +169,7 @@
 
       // echo $link;
 
-      $next = 'http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/?&logout=true';
+      $next = 'http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/index.php?&logout=true';
       $link = $helper->getLogoutUrl($sess,$next);
 
       $cookie_name="logoutlink1";
