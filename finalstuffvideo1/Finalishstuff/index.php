c43990bbb6ab6a4a8939e9e865a3335edcab4a19
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-02-27 19:02:39 +0530

    logout changes

--- finalstuffvideo1/Finalishstuff/index.php
+++ finalstuffvideo1/Finalishstuff/index.php
@@ -199,7 +199,7 @@
         //connect to the datatbase
         mysql_select_db("webrtc");
 
-        $query = "INSERT INTO users (firstname,lastname,fullname,emailid,hashemail,username,password,country,gender,imagename,image,imagelarge,imagesmall,status) VALUES('$fname','$lname','$fullname','$em','$a','$uname','','$country','$gender','','','$largeimage','$smallimage','Online')"; //check if it works!!!!
+        $query = "INSERT INTO users (firstname,lastname,fullname,emailid,hashemail,username,password,country,gender,imagename,image,imagelarge,imagesmall,status) VALUES('$fname','$lname','$fullname','$email','$a','$uname','','$country','$gender','','','$largeimage','$smallimage','Online')"; //check if it works!!!!
 
         $result = mysql_query($query);
 
