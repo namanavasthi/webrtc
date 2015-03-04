028b8911a795637227abb3104c92f46879e63cca
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-03-04 23:26:02 +0530

    final changes

--- finalstuffvideo1/Finalishstuff/notifications.php
+++ finalstuffvideo1/Finalishstuff/notifications.php
@@ -122,7 +122,7 @@ function refresh()
 							<span class="glyphicon glyphicon-comment"></span>
 							Notification
 						<!-- </button> -->
-						<span class="badge badge-notify"><?php echo $count; ?></span>
+						<span class="badge badge-notify"><?php echo "$count"; ?></span>
 					<!-- </div> -->
 					</a>
 				</li>
