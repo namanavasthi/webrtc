e8acc35b2553f7268abc4184f9b215ea6f08edfd
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-03-06 01:43:15 +0530

    final changes

--- finalstuffvideo1/Finalishstuff/multichat.php
+++ finalstuffvideo1/Finalishstuff/multichat.php
@@ -108,12 +108,14 @@ label {
 
 .regular-checkbox + label:active, .regular-checkbox:checked + label:active {
 	box-shadow: 0 1px 2px rgba(0,0,0,0.05), inset 0px 1px 3px rgba(0,0,0,0.1);
+	top: 25px;
 }
 
 .regular-checkbox:checked + label {
 	background-color: #e9ecee;
 	border: 1px solid #adb8c0;
 	box-shadow: 0 1px 2px rgba(0,0,0,0.05), inset 0px -15px 10px -12px rgba(0,0,0,0.05), inset 15px 10px -12px rgba(255,255,255,0.1);
+	top: 25px;
 	color: #99a1a7;
 }
 
@@ -121,7 +123,7 @@ label {
 	content: '\2714';
 	font-size: 14px;
 	position: absolute;
-	top: 0px;
+	top: 25px;
 	left: 3px;
 	color: #99a1a7;
 }
@@ -185,6 +187,7 @@ label {
 	background-color: #fafafa;
 	border: 1px solid #cacece;
 	box-shadow: 0 1px 2px rgba(0,0,0,0.05), inset 0px -15px 10px -12px rgba(0,0,0,0.05);
+	top: 25px;
 	padding: 9px;
 	border-radius: 50px;
 	display: inline-block;
