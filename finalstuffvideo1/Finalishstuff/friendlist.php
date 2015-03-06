4a981191618cb746ac930fc4445ce1495cf7ddd5
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-03-06 15:10:28 +0530

    final changes

--- finalstuffvideo1/Finalishstuff/friendlist.php
+++ finalstuffvideo1/Finalishstuff/friendlist.php
@@ -1,3 +1,8 @@
+<?php
+
+include('search.php');
+
+?>
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 	<head>
@@ -14,11 +19,134 @@
 		<!--[if IE]>
   		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
+
+				<!-- bootstrap scripts -->
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+
+<!-- end of bootstrap scripts -->
+
+
+
+<style>
+			.badge-notify{
+			   background:red;
+			   position:relative;
+			   top: -15px;
+			   /*left: -35px;*/
+			}
+
+		</style>
+
+<style>
+  .navbar-brand img{
+    width: 45px;
+    height: 35px;
+    margin-top: -7px;
+    margin-right: 3px;
+    float: left;
+  }
+</style>
+
+
+
+
+
 	</head>
 	<body>
 
+
+
+<!-- navbar header -->
+
+	<div class="navbar-wrapper" id="nav_re">
+  <div class="container">
+    <div class="navbar navbar-fixed-top navbar-inverse navbar-static-top">
+      
+        <div class="navbar-header">
+	    <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	      <span class="icon-bar"></span>
+	      <span class="icon-bar"></span>
+	      <span class="icon-bar"></span>
+	    </a>
+        <a class="navbar-brand" href="http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/index.php"><img src="images/webrtc.png">Vid3Com</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/homepage.php">Home</a></li>
+            <li><a href="http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/aboutus/aboutus.html" target="ext">About</a></li>
+            <li><a href="http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/contactus.php">Contact</a></li>
+            <li>
+				<!-- <div class="container"> -->
+				<a href="http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/notifications.php">
+					<!-- <button class="btn btn-default btn-lg btn-link" style="font-size:36px;"> -->
+						<span class="glyphicon glyphicon-comment"></span>
+						Notification
+					<!-- </button> -->
+					<!-- <span class="badge badge-notify"> -->
+						<?php 
+							mysql_connect("127.2.139.130","adminPfy2zVu","BXXbBfmR7fWS");
+							mysql_select_db("webrtc");
+							$name=$_COOKIE['userdata']['email'];
+							//$name='emma@gmail.com';
+							$query = mysql_query("SELECT * FROM notifications WHERE username='$name' AND viewstatus<>'seen'");
+							$count=mysql_num_rows($query);
+							if ($count!=0) {
+									echo "<span class='badge badge-notify'>$count</span>";
+								}
+						?>
+					<!-- </span> -->
+				<!-- </div> -->
+				</a>
+			</li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+          	<?php
+				$cookie_name="logoutlink1";
+				if(!isset($_COOKIE[$cookie_name])) {
+				    echo "<li><a href='http://webrtc-fypgroup11.rhcloud.com/finalstuffvideo1/Finalishstuff/' alt='Logout'><span class='glyphicon glyphicon-off' aria-hidden='true'></span>Logout&nbsp;&nbsp;</a></li>";
+				} else {
+				    $link=$_COOKIE[$cookie_name];
+				}
+				echo "<li><a href='".$link."' alt='Logout from Facebook'><span class='glyphicon glyphicon-off' aria-hidden='true'></span>Logout&nbsp;&nbsp;</a></li>";
+			?>
+			</ul>
+        </div>
+
+    </div>
+  </div><!-- /container -->
+</div><!-- /navbar wrapper -->
+
+<!-- end of navbar -->
+
+
+
+
+
+<br>
+<br>
+<br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		<?php
-			include('search.php');
+			
 
 			//$sval=$_GET["sval"];
 			//print $sval;
