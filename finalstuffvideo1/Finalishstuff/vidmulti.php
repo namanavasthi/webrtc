fb96c0072e235f0f6c0c70fa6c77343175a9f7c0
NAMAN AVASTHI <naman.avasthi@gmail.com>
2015-03-06 12:14:22 +0530

    final changes

--- finalstuffvideo1/Finalishstuff/vidmulti.php
+++ finalstuffvideo1/Finalishstuff/vidmulti.php
@@ -325,7 +325,15 @@ function goBack() {
                             
                          
                          ";     
-                         echo"<button onclick='goBack()'>Click to select participants </button>   "; 
+                         // echo"<button onclick='goBack()'>Click to select participants </button>   "; 
+
+                         echo "
+                            <div class='btn-group btn-group-justified' role='group' aria-label='...''>
+                                <div class='btn-group' role='group'>
+                                    <button type='button' class='btn btn-default' onclick='goBack()'>Click to select participants</button>
+                                </div>
+                            </div>
+                         ";
 
 
                     }
